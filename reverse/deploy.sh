eb0c4c207a9d9ed30d8949a017c7477aff38fd6e
Josh Long <starbuxman@gmail.com>
2018-04-05 00:00:10 -0700

    reverse

--- reverse/deploy.sh
+++ reverse/deploy.sh
@@ -5,7 +5,7 @@ mvn clean package
 riff create java -a target/reverse-0.0.1-SNAPSHOT.jar --force \
     --input reverse \
     --output uppercase \
-    --name uppercase \
+    --name reverse \
     --handler "reverse&main=com.example.reverse.ReverseApplication"
 
 
